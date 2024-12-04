faa054665061bca89110250481dae2eeb115f387
666sniperererer <666sniperererer@gmail.com>
2024-12-04 19:31:51 +0100

    doplněny příklady logických operátorů vložené do stringu

---
+++ logicke_operatory.py
@@ -0,0 +1,9 @@
+print(f"True or False: {True or False}")
+print(f"False or False: {False or False}")
+print(f"False and False: {False and False}")
+print(f"True and False: {True and False}")
+print(f"True and True: {True and True}")
+print(f"True and False or True: {True and False or True}")
+print(f"True and False or False: {True and False or False}")
+print(f" 50 > 40 or 40 > 50: {50 > 40 or 40 > 50}") #True
+print(f" 50 > 40 and 40 > 50: {50 > 40 and 40 > 50}") #False
