257cc1569150d69d1eb084957708325d409cef9c
666sniperererer <666sniperererer@gmail.com>
2024-12-04 19:24:05 +0100

    doplněny příklady operátorů

--- prvni_skrypt.py
+++ prvni_skrypt.py
@@ -6,9 +6,18 @@ print("třetí řádek pro třetí commit")
 
 #formátování proměnných do stringu
 jmeno = "Honza"
+jmenotrikrat = jmeno+jmeno+jmeno
 nick = "666sniperererer"
 cislo = 2/3
 
-print(f"Ahoj, mé jméno je {jmeno}")
+print("Ahoj, mé jméno je {jmeno}") #chybí f, složené závorky jsou pouze text
+print(f"Ahoj, mé jméno je {jmeno}") #na začátku je f, složené závorky fungují pro definici proměnné
 print(f"Můj nick je {nick} a .2f formátované číslo 2/3 je {cislo:.2f}")
 print(f"Můj nick je {nick} a .4f formátované číslo 2/3 je {cislo:.4f}")
+
+print(jmenotrikrat)
+print(jmeno==jmeno)
+print(jmeno==jmenotrikrat)
+print(6>7)
+print(1>cislo)
+print(cislo>1)
