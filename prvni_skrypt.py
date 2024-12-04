f3bf7545fef121000974b9854b45b3e5243c6684
666sniperererer <666sniperererer@gmail.com>
2024-12-04 19:15:18 +0100

    doplněny příklady formátování proměnných do txt stringu

--- prvni_skrypt.py
+++ prvni_skrypt.py
@@ -3,3 +3,12 @@ print("test GITu")
 print("druhý řádek pro druhý commit")
 
 print("třetí řádek pro třetí commit")
+
+#formátování proměnných do stringu
+jmeno = "Honza"
+nick = "666sniperererer"
+cislo = 2/3
+
+print(f"Ahoj, mé jméno je {jmeno}")
+print(f"Můj nick je {nick} a .2f formátované číslo 2/3 je {cislo:.2f}")
+print(f"Můj nick je {nick} a .4f formátované číslo 2/3 je {cislo:.4f}")
